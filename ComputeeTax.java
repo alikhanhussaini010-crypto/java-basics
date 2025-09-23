1befbe922a4e6cdb5099395f923080ee7d4df885
alikhanhussaini010-crypto <alikhanhussaini010@gmail.com>
2025-09-23 04:16:17 -0700

    Add files via upload

---
+++ ComputeeTax.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+public class ComputeeTax {
+    public static void main(String[] args) { 
+       Scanner input = new Scanner(System.in);
+       
+      System.out.print("0 for single filer: Enetr the filing staus:");
+      int status=input.nextInt();
+      
+      System.out.print("Enetr the taxable income:");
+      double income=input.nextDouble();
+      
+      double tax =0;
+      if(status==0){
+          if(income<=8350)
+              tax=income*0.10;
+          else if(income<=33950)
+              tax=8350*0.10+(income-8350)*0.15;
+          else if(income<=82250)
+              tax=8350*0.10 +(33950-8350)*0.15+(income-33950)*0.25;
+          else if (income<=171550)
+              tax=8350*0.10+(33950-8350)*0.15+(82250-33950)*0.25+(income-82250)*0.28;
+          else if(income<=372950)
+              tax = 8350*0.10+(33950-8350)*0.15+(82250-33950)*0.25+(171550-82250)*0.28+(income-171550)*0.33;
+          else
+                 tax = 8350*0.10+(33950-8350)*0.15+(82250-33950)*0.25+(171550-82250)*0.28+
+                         (372950-171550)*0.33+(income-372950)*0.35;
+          System.out.println("the tax is"+tax);
+               
+      }
+      else
+            System.out.println("the filing status you have eneterd is wrong:");
+      
+      
+      
+      
+      
+    }
+    
+}
